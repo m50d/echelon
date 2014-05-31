f7c9c51b768cb03509cdb5ebdd60ac2bba75b1a3
Michael Donaghy <md401@srcf.ucam.org>
2014-06-01 00:56:36 +0100

    hmm
    Signed-off-by: Michael Donaghy <md401@srcf.ucam.org>

--- src/main/scala/net/homelinux/md401/echelon/Echelon.scala
+++ src/main/scala/net/homelinux/md401/echelon/Echelon.scala
@@ -2,6 +2,15 @@ package net.homelinux.md401.echelon
 
 import shapeless._
 
+trait IsReducedEchelonForm[MT]
+
+object IsReducedEchelonForm {
+  implicit object EmptyIsReducedEchelonForm extends IsReducedEchelonForm[HNil]
+
+  implicit def AddRowReducedEchelonForm[RT <: HNil] = new IsReducedEchelonForm[RT] {}
+}
+
 object Echelon {
-	def solve[MT <: HList](matrix: MT) = matrix
+
+  def solve[MT <: HList](matrix: MT)(implicit w: IsReducedEchelonForm[MT]) = matrix
 }
