cb60ff78971acd9cf5c51a4423b4aa17f26b4b6d
Michael Donaghy <md401@srcf.ucam.org>
2014-06-01 01:33:35 +0100

    umm
    Signed-off-by: Michael Donaghy <md401@srcf.ucam.org>

--- src/main/scala/net/homelinux/md401/echelon/Echelon.scala
+++ src/main/scala/net/homelinux/md401/echelon/Echelon.scala
@@ -1,13 +1,28 @@
 package net.homelinux.md401.echelon
 
 import shapeless._
+import scala.None
+
+trait ColumnExtension[MT] {
+  type Extended <: HNil
+}
+
+object ColumnExtension {
+  implicit object EmptyColumnExtension extends ColumnExtension[HNil] {
+    type Extended = HNil
+  }
+  
+  implicit def AddRowColumnExtension[RT <: HNil, TT <: HNil, ETT <: HNil] = new ColumnExtension[RT :: TT] {
+    type Extended = (Int :: RT) :: ETT
+  }
+}
 
 trait IsReducedEchelonForm[MT]
 
 object IsReducedEchelonForm {
   implicit object EmptyIsReducedEchelonForm extends IsReducedEchelonForm[HNil]
 
-  implicit def AddRowReducedEchelonForm[RT <: HNil] = new IsReducedEchelonForm[RT] {}
+  implicit def AddRowReducedEchelonForm[TT <: HNil] = new IsReducedEchelonForm[TT] {}
 }
 
 object Echelon {
